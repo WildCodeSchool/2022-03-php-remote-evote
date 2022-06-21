d9b1e9b9ae2cd4ae9b334514dfcc1551f902ea29
J007B <c.fougeroux007@gmail.com>
2022-06-21 07:57:01 +0200

    add layout file

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -7,7 +7,6 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-
 class LoginController extends AbstractController
 {
     #[Route('/login', name: 'login')]
